f4e2366892fccd32efe8f0f828721558ee165947
Casper <solitaryreach@163.com>
2019-06-12 13:01:43 +0800

    添加进度条异步任务

--- src/main/java/org/r/idea/plugin/generator/api/Generate.java
+++ src/main/java/org/r/idea/plugin/generator/api/Generate.java
@@ -2,14 +2,9 @@ package org.r.idea.plugin.generator.api;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import java.util.ArrayList;
-import java.util.Arrays;
-import org.r.generator.api.core.Generator;
-import org.r.generator.api.core.beans.ConfigBean;
-import org.r.idea.plugin.generator.api.beans.SettingState;
-import org.r.idea.plugin.generator.api.service.StorageService;
+import org.r.idea.plugin.generator.api.task.BuildTask;
 
 /**
  * @ClassName Generate
@@ -18,30 +13,12 @@ import org.r.idea.plugin.generator.api.service.StorageService;
  **/
 public class Generate extends AnAction {
 
-    private StorageService storageService = StorageService.getInstance();
-
-
     @Override
     public void actionPerformed(AnActionEvent e) {
 
         Project project = e.getProject();
-        SettingState state = storageService.getState();
-        String splitChar = ",";
-        if (state == null || project == null) {
-            throw new RuntimeException("程序异常");
-        }
-        ConfigBean configBean = new ConfigBean();
-        configBean.setProjectPath(project.getBasePath());
-        configBean.setSrcFilePath(new ArrayList<>(Arrays.asList(state.getSrcFilePaths().split(splitChar))));
-        configBean.setInterfacePath(new ArrayList<>(Arrays.asList(state.getInterfaceFilePaths().split(splitChar))));
-        configBean.setCodeTemplatePath(state.getTemplateFilePaths());
-        configBean.setDocPath(state.getOutputFilePaths());
-        configBean.setBaseClassType(state.getBaseClass().split(splitChar));
-        configBean.setCollectionClassType(state.getCollectionClass().split(splitChar));
-        configBean.setNonsupportCollectionClassType(state.getNonsupportClass().split(splitChar));
+        ProgressManager.getInstance().run(new BuildTask(project, "build doc"));
 
-        Generator generator = new Generator();
-        generator.generate(configBean);
     }
 
 
