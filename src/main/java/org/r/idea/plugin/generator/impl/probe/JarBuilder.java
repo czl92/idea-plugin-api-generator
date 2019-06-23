f417dce063a186e53a2d0c6b70546573522403c3
casper <solitaryreach@163.com>
2019-06-23 22:25:04 +0800

    添加容器jar

--- src/main/java/org/r/idea/plugin/generator/impl/probe/JarBuilder.java
+++ src/main/java/org/r/idea/plugin/generator/impl/probe/JarBuilder.java
@@ -1,5 +1,7 @@
 package org.r.idea.plugin.generator.impl.probe;
 
+import java.util.List;
+
 /**
  * @Author Casper
  * @DATE 2019/6/23 21:09
@@ -11,19 +13,41 @@ public class JarBuilder {
      */
     private String contarinerJar = "/container.jar";
 
-    private String productJar = "";
+    /**
+     * 目标可运行jar包
+     */
+    private String productJar = "api-doc.jar";
 
-    public void build(String srcDir){
+    /**
+     * 临时的源文件目录
+     */
+    private String tmpJavaDir = "java/";
 
-        /*编译源文件*/
+    /**
+     * 临时的class文件目录
+     */
+    private String tmpClassDir = "class/";
+
+
+    public void build(String srcDir, String outpuParh) {
+
+        /*编译源文件,并储存为临时文件*/
 
         /*copy容器*/
 
-
+        /*清理临时文件*/
 
     }
 
-
+    /**
+     * 编译源文件并输入到指定的临时目录
+     *
+     * @param srcDir 源文件的目录路径
+     * @return
+     */
+    private List<String> compile(String srcDir) {
+        return null;
+    }
 
 
 }
